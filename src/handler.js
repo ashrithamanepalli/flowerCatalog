8a573f94c720b96bb455f927c6b998d38f0fc805
Ashritha M <99526903+ashrithamanepalli@users.noreply.github.com>
2022-06-27 18:04:58 +0530

    Added guestBook for the app

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,29 @@
+const fs = require('fs');
+
+const displayComments = (response) => {
+  let stringComments = '';
+  const comments = JSON.parse(fs.readFileSync('./public/comments.json', 'utf-8'));
+  comments.forEach(({ date, name, comment }) => {
+    stringComments += `Date : ${date} Name : ${name} Comment : ${comment}\n`
+  });
+  response.send(stringComments);
+};
+
+const storeComments = (request) => {
+  const date = new Date;
+
+  const existingComments = JSON.parse(fs.readFileSync('./public/comments.json', 'utf-8'));
+  existingComments.unshift({ date: date, name: request.name, comment: request.comment });
+
+  fs.writeFileSync('./public/comments.json', JSON.stringify(existingComments));
+  return;
+}
+
 const basicHandler = (request, response) => {
   const { uri } = request;
-  if (uri === '/downloadAbeliophyllum') {
-    response.send('Abeliophyllum');
-    return true;
-  }
-
-  if (uri === '/downloadAgeratum') {
-    response.send('Ageratum');
+  if (uri === '/store-comment') {
+    storeComments(request);
+    displayComments(response);
     return true;
   }
   return false;
