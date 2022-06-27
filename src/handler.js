c15d7d05b945cb10314d715332c934de9d64958b
Ashritha M <99526903+ashrithamanepalli@users.noreply.github.com>
2022-06-27 23:48:20 +0530

    Made comments appear in the same page of the guest page

--- src/handler.js
+++ src/handler.js
@@ -1,37 +1,51 @@
 const fs = require('fs');
 
-const displayComments = (response) => {
+const formatComment = ({ date, name, comment }) => {
   let stringComments = '';
-  const comments = JSON.parse(
-    fs.readFileSync('./public/comments.json', 'utf-8'));
-  comments.forEach(({ date, name, comment }) => {
-    const parsedComments = comment.replace('+', ' ');
-    stringComments +=
-      `Date : ${date} Name : ${name} Comment : ${parsedComments}\n`
-  });
+  const parsedComments = comment.replace('+', ' ');
+  const formattedComment =
+    `Date : ${date} Name : ${name} Comment : ${parsedComments}`;
+  stringComments += `<li>${formattedComment}</li>`;
 
-  response.send(stringComments);
+  return stringComments;
 };
 
-const storeComments = (request) => {
-  const date = new Date;
+const displayComments = (comments, response) => {
+  const formattedComments = comments.map(formatComment).join('');
+
+  const template = fs.readFileSync('./public/guestBookTemplate.html', 'utf-8');
+  const mainPage = template.replace('__COMMENTS__', formattedComments);
+  fs.writeFileSync('./public/guestBook.html', mainPage, 'utf-8');
+
+  response.send(mainPage);
+};
+
+const storeComments = (request, response) => {
+  const date = new Date().toLocaleString();
 
   const existingComments = JSON.parse(
     fs.readFileSync('./public/comments.json', 'utf-8'));
-  existingComments.unshift(
-    { date: date, name: request.name, comment: request.comment });
 
+  existingComments.unshift({
+    date: date,
+    name: request.name,
+    comment: request.comment
+  });
+
+  displayComments(existingComments, response);
   fs.writeFileSync('./public/comments.json', JSON.stringify(existingComments));
+
   return;
-}
+};
 
 const basicHandler = (request, response) => {
-  const { uri } = request;
+  let { uri } = request;
+
   if (uri === '/store-comment') {
-    storeComments(request);
-    displayComments(response);
+    storeComments(request, response);
     return true;
   }
+
   return false;
 };
 
