c39eedeff7a230691961cc50d16f42acbde44de9
Ashritha M <99526903+ashrithamanepalli@users.noreply.github.com>
2022-07-01 09:55:28 +0530

    Made the serveFileContentHandler async

--- src/handlers/serveFileContent.js
+++ src/handlers/serveFileContent.js
@@ -1,60 +1,27 @@
 const fs = require('fs');
-const path = require('path');
-
-const contentTypes = {
-  '.jpg': 'text/jpeg',
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.jpg': 'image/jpeg',
-  '.pdf': 'application/pdf'
-};
-
-const getContentType = (extension) => {
-  return contentTypes[extension] || 'text/plain';
-};
-
-const readFiles = (directory) => {
-  let contents = {};
-  const files = fs.readdirSync(directory);
-
-  files.forEach((file) => {
-    const filePath = `${directory}/${file}`;
-    if (!fs.existsSync(filePath)) {
-      return false;
-    }
-
-    if (fs.statSync(filePath).isDirectory()) {
-      contents = { ...contents, ...readFiles(filePath) };
-    } else {
-      const content = fs.readFileSync(filePath);
-      contents[filePath] = content;
-    }
-  }
-  )
-  return contents;
-}
+const mime = require('mime-types');
 
 const alias = {
   '/': '/welcome.html'
 };
 
 const createFileContentServer = (root = './public') => {
-  const contents = readFiles(root);
-
-  return (request, response) => {
+  return (request, response, next) => {
     const { pathname } = request.url;
     const uri = alias[pathname] || pathname;
     const fileName = root + uri;
 
-    if (!fs.existsSync(fileName)) {
-      return false;
-    }
-
-    response.setHeader('Content-type', getContentType(path.extname(fileName)));
-    response.end(contents[fileName]);
-    return true;
+    fs.exists(fileName, (exists) => {
+      if (exists) {
+        response.setHeader('Content-type', mime.lookup(fileName));
+        fs.readFile(fileName, (err, data) => {
+          response.end(data);
+        });
+        return true;
+      }
+      next();
+    });
   };
-
-}
+};
 
 module.exports = { createFileContentServer };
